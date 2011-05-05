b76873be0c553a5f8359e1a12438ee37d4c0ed3e
sbuckpesch <s.buckpesch@iconsultants.eu>
2011-05-05 21:15:45 +0100

    Delete Zend V2 submodule add init-content

--- init.php
+++ init.php
@@ -1,1 +1,35 @@
 <?php
+error_reporting(E_ALL|E_STRICT);
+ini_set('display_errors', 1);
+date_default_timezone_set('Europe/Berlin');
+
+// Load config
+require_once dirname(__FILE__).'/config.php';
+
+//set include path
+define("ROOT_PATH",realpath(dirname(__FILE__)));
+set_include_path(ROOT_PATH.'/lib/' . PATH_SEPARATOR .
+  ROOT_PATH.'/modules/' . PATH_SEPARATOR .
+  get_include_path());
+  
+// Register Zend autoload
+require_once "Zend/Loader/Autoloader.php";
+Zend_Loader_Autoloader::getInstance()->setFallbackAutoloader(true);
+
+//setup database
+$db_config=array(
+  'host'=>$database_host,
+  'username'=>$database_user,
+  'password'=>$database_pass,
+  'dbname'=>$database_name
+);
+
+$db = Zend_Db::factory('mysqli',$db_config);
+$db->query('set names utf8');
+Zend_Db_Table::setDefaultAdapter($db);
+
+//set global variable
+$global=new ArrayObject();
+$global->db=$db;
+
+?>
