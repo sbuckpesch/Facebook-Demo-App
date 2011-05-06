a3352725d47d2dedf4656c98f5a8402a0faf63e0
sbuckpesch <s.buckpesch@iconsultants.eu>
2011-05-06 17:51:07 +0100

    Update comments and explanations for home page

--- index.php
+++ index.php
@@ -6,16 +6,18 @@ include 'header.php';
 
 /*
  * Integrate a Content from App-Arena App-Manager
- * Three-Arrays of Content-Elements are available
- * 1. $global->app['content'] 	--> All content elements
- * 2. $global->app['config'] 	--> All configuration values  
- * 3. $global->app['content']	--> All Design elements
- * The name in the second bracket is the "identifier" which has been setup in App-Manager. 
+ * Four Arrays of Content-Elements are available
+ * 1. $global->app['instance'] 	--> All Instance Base Data
+ * 2. $global->app['content'] 	--> All content elements
+ * 3. $global->app['config'] 	--> All configuration values  
+ * 4. $global->app['design']	--> All Design elements
+ * The name in the second bracket is the "identifier" which has been setup in App-Manager.
+ * E.g. echo $global->app['content']['home']; --> shows the App-Manager content-element with the identifier "home" 
+ * To show all available data just uncomment the following line 
  */
-$global->app['content']['content_test'];
+//echo '<h1>All available App-Arena data</h1><pre>', print_r($global->app), '</pre>';
 
-// To show all available data just uncomment the following line
-echo '<pre>', print_r($global->app), '</pre>';
+echo $global->app['content']['home'];
 
 // Include Footer Part
 include 'footer.php';
