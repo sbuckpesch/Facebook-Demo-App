3b3bd377de46aed2f56f4c9f7fe54b6124adeefd
fred <fred@iconsultants.eu>
2011-05-06 23:19:06 +0800

    test

--- init.php
+++ init.php
@@ -42,8 +42,6 @@ $apparena = new Client($aa_api_key);
 $instance_id = 0;
 $page_id=get_page_id();
 
-var_dump($aa_app_id);
-var_dump($page_id);exit();
 $apparena->setInstanceId($aa_app_id, $instance_id,$page_id);
 $app = $apparena->getData();
 $global->app = $app;
