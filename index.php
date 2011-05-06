5de734048325be9ea187cc15258817206a2590b6
sbuckpesch <s.buckpesch@iconsultants.eu>
2011-05-06 17:42:40 +0100

    Add demo text

--- index.php
+++ index.php
@@ -1,28 +1,22 @@
 <?php
 // Load all config values and necessary classes
 require_once ('init.php');
-
 // Include Header Part
 include 'header.php';
-?>
 
-<h1>App-Arena Demo Applikation</h1>
+/*
+ * Integrate a Content from App-Arena App-Manager
+ * Three-Arrays of Content-Elements are available
+ * 1. $global->app['content'] 	--> All content elements
+ * 2. $global->app['config'] 	--> All configuration values  
+ * 3. $global->app['content']	--> All Design elements
+ * The name in the second bracket is the "identifier" which has been setup in App-Manager. 
+ */
+$global->app['content']['content_test'];
 
-<p>
-Klick dich durch die Applikation und sehe was du nun alles kannst.
-</p>
+// To show all available data just uncomment the following line
+echo '<pre>', print_r($global->app), '</pre>';
 
-<<<<<<< HEAD
-<p>Config-Element: <?php print_r($global->app);?></p> 
-<!--<p>Content-Element: <?=$global->app['content']['content_test']?></p>
-=======
-<p>Config-Element: <?=$global->app['config']['config_test']?></p>
-<p>Content-Element: <?=$global->app['content']['content_test']?></p>
->>>>>>> 42e61fcf1ac04bc2f8d294ab8cba28625ae45748
-<p>Design-Element: not implemented yet</p>
-
-
---><?php 
 // Include Footer Part
 include 'footer.php';
 ?>
